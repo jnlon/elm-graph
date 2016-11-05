1b7119a8acdba65d9a19d90393b695a7afb11890
spinclick <spinclick@users.noreply.github.com>
2016-11-04 22:39:47 -0400

    Get bars on graph

--- Visual.elm
+++ Visual.elm
@@ -19,14 +19,15 @@ range from to step =
   in
       List.reverse <| genRange from []
 
-getBarColor ith = 
+getBarColour ith = 
   let colours = Array.fromList [red,blue,green] in
   Maybe.withDefault red <| 
     Array.get (ith % Array.length colours ) <| colours
 
 -- boundaries
-b =  {left = -450, right = 450, top = 250, bottom = -250}
-makeBars barEntry = 1
+b = {left = -450.0, right = 450.0, top = 250.0, bottom = -250.0}
+graphHeight = (abs b.bottom) + b.top
+graphWidth =  (abs b.left) + b.right
 smallText t = filled black <| size 9 t 
 
 -- assumes minimum is zero
@@ -37,15 +38,14 @@ makeGraph yMax =
       yMakeGrid y = line (b.left,y) (b.right,y) |> outlined (solid 1) grey
       yMakeLine y = line (b.left,y) (b.right,y) |> outlined (solid 2) black
       yLabels = 10 
-      height = (abs(b.bottom)+b.top)
       yMakeLabel i y = 
         group 
           [ yMakeLine y
           , text (toString ((toFloat i)*(yMax/yLabels))) |> smallText |> move ((b.left-35),(y+1))]
       yLabelLines =  
-        List.indexedMap yMakeLabel (range b.bottom b.top (height/yLabels))
+        List.indexedMap yMakeLabel (range b.bottom b.top (graphHeight/yLabels))
       yGrid = 
-        List.map yMakeGrid (range b.bottom b.top (height/yLabels/5))
+        List.map yMakeGrid (range b.bottom b.top (graphHeight/yLabels/5))
   in 
   group 
     [ group yGrid
@@ -53,8 +53,26 @@ makeGraph yMax =
     , leftLine |> outlined (solid 5) black 
     , bottomLine |> outlined (solid 5) black ]
 
+
+makeBars data maxHeight numBars = 
+  let 
+      barWidth = graphWidth/(toFloat (List.length data))
+      toHeight n = (n/maxHeight)*graphHeight
+      -- I DON't KNOW HOW THIS WORKS
+      toXPosition i = ((i/numBars)*graphWidth) + (b.left + barWidth/2)
+      makeBar i d = 
+        rect barWidth (toHeight (fst d))
+          |> filled (getBarColour i) 
+          |> move ((toXPosition (toFloat i)), b.bottom) 
+  in
+      group <| List.indexedMap makeBar data
+
 graph = 
-  makeGraph 500
+  let data = [(10, "test"), (2, "two"), (5, "two")]
+      maxHeight = (fst (Maybe.withDefault (0,"") (List.maximum data)))
+  in
+  group [  makeBars data maxHeight (toFloat (List.length data))
+         , makeGraph 10 ]
 
 --main = Html.text (toString (range 10 30 2)) 
 main = graphicsApp { view = collage 1000 500 [graph] }
