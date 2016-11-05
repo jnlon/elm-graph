2491c44f23ed0e6688ca819327f9b856a06d68fa
spinclick <spinclick@users.noreply.github.com>
2016-11-05 17:45:23 -0400

    Make labels a bit nicer

--- Visual.elm
+++ Visual.elm
@@ -21,12 +21,12 @@ range from to step =
       List.reverse <| genRange from []
 
 getBarColour ith = 
-  let colours = Array.fromList [red,blue,green] in
+  let colours = Array.fromList [red,blue,green,yellow,purple] in
   Maybe.withDefault red <| 
     Array.get (ith % Array.length colours ) <| colours
 
 -- boundaries
-b = {left = -450.0, right = 450.0, top = 250.0, bottom = -190.0}
+b = {left = -450.0, right = 450.0, top = 250.0, bottom = -120.0}
 graphHeight = (abs b.bottom) + b.top
 graphWidth =  (abs b.left) + b.right
 smallText t = filled black <| size 9 t 
@@ -67,19 +67,20 @@ makeBars data maxHeight numBars =
       makeBar i d = 
         let barHeight = (toHeight (fst d))
             barXPosition = toXPosition (toFloat i)
-            xLabel = text (snd d) |> filled black 
+            xLabel = text (snd d) |> size 8 |> filled black 
             barSizeLabel = text (String.left 5 (toString (fst d))) |> filled black
             xAxisTextX = barXPosition-10
-            xAxisTextPosition = if (i % 2) == 0 
-            then (xAxisTextX, b.bottom-20) 
-            else (xAxisTextX, b.bottom-45)
+            xAxisText = 
+              case (i % 2) of
+                1 ->  xLabel |> move (xAxisTextX, b.bottom-20) |> rotate (degrees 40)
+                o -> xLabel |> move (xAxisTextX, barHeight)  |> rotate (degrees 90)
         in
         group 
           [ rect barWidth barHeight
               |> filled (getBarColour i) 
               |> move (barXPosition, (b.bottom+(barHeight/2))) 
-          , xLabel |> move xAxisTextPosition
-          , barSizeLabel |> move xAxisTextPosition |> move (8,-12)]
+          , xAxisText ]
+          --, barSizeLabel |> move xAxisTextPosition |> move (8,-12)]
       in
       group <| List.indexedMap makeBar data
 
